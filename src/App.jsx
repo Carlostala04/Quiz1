14954d75da2c42a1c6298835852015305eefa404
clarke021 <donobanclarke22@gmail.com>
2026-03-05 17:56:47 -0600

    feat: componente LoadingSpinner con estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import LoadingSpinner from "./LoadingSpinner";
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <LoadingSpinner message="Cargando personajes..." />
+    </div>
+  );
 }
 
-export default App
+export default App;
