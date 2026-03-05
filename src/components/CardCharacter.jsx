f4d2a4efd79c764332e5ae8a3770280b4827df05
Carlostala04 <carlostala.dev@gmail.com>
2026-03-05 17:47:10 -0600

    se creo la estructura que tendran las cards

---
+++ src/components/CardCharacter.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../styles/CardCharacter.css";
+function CardCharacter({
+  nombre,
+  status,
+  especie,
+  gender,
+  origen,
+  urlImage,
+  creacion,
+}) {
+  const getDate =
+    new Date(creacion).getDay() +
+    "-" +
+    new Date(creacion).getMonth() +
+    "-" +
+    new Date(creacion).getFullYear();
+  return (
+    <div className="card">
+      <section className="character-img">
+        <img src={`${urlImage}`} alt={`imagen de ${nombre}`} />
+      </section>
+      <section className="character-info">
+        <h2 className="character-name">Name: {nombre}</h2>
+        <span className="character-status">Status: {status}</span>
+        <span className="character-especie">Species: {especie}</span>
+        <span className="character-gender">Gender: {gender}</span>
+        <span className="character-origen">Origin: {origen}</span>
+        <span>Date created: {getDate}</span>
+      </section>
+    </div>
+  );
+}
+
+export default CardCharacter;
