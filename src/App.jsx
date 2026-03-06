9ed8c6c4db836e09f8b61c3b22bff74afd111f3c
jansmv <rachellgodoy69@gmail.com>
2026-03-05 18:24:25 -0600

    feat: integrar hook en App y agregar campo de búsqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
-import './App.css'
+import { useState } from "react";
+import useRickAndMorty from "./hooks/UseRickAndMorty";
 
 function App() {
-  
+  const [search, setSearch] = useState("");
+  const { characters, loading, error } = useRickAndMorty(search);
 
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar personaje..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {loading && <p>Cargando...</p>}
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
