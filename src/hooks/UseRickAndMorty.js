a37bc9782efc918d8b1faba208f15b4fcf1925ae
jansmv <rachellgodoy69@gmail.com>
2026-03-05 18:19:52 -0600

    feat: agregar lógica de fetch y useEffect al hook

--- src/hooks/UseRickAndMorty.js
+++ src/hooks/UseRickAndMorty.js
@@ -4,6 +4,20 @@ const useRickAndMorty = (search) => {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    fetch(`https://rickandmortyapi.com/api/character/?name=${search}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setLoading(false);
+      });
+  }, [search]);
+
+  return { characters, loading, error };
 };
 
 export default useRickAndMorty;
